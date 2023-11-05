3fe4cac8d1284a48396e989a006cab24b54a3a8c
longtx <long.tx@seventh-pitch.com>
2023-11-05 23:43:41 +0700

    create base nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
+@Controller('login')
+@ApiTags('') // Đánh dấu tag cho controller
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/getCats')
+  @ApiOperation({ summary: '' })
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
